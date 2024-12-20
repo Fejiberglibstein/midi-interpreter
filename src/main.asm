babda9aea7cf4993fd2e75e306d654fadf469f63
Austin Thein <amthein321@gmail.com>
2024-12-20 18:17:47 -0500

    Fix some stuff up

--- src/main.asm
+++ src/main.asm
@@ -1,11 +1,11 @@
 .data
+	## Name of the midi file to read from. In the future this should be changed
+	## to read from user input for file.
+	FileName: .asciiz "../examples/test.mi"
+
 	## Allocate region for 16 channels. Each channel is 2 bytes, one byte for
 	## instrument, and one byte for volume
 	Channels: .space 32 # 2 bytes * 16 channels
-
-	## Name of the midi file to read from. In the future this should be changed
-	## to read from user input for file.
-	FileName: .asciiz "../examples/test.mid"
 
 	## All the header chunks for a midi file are 4 bits for the chunk type, 4
 	## bytes for chunk length, and 6 bytes for the header's content.
@@ -13,46 +13,52 @@
 	## All midi header chunks are _always_ 6 bytes
 	HeaderChunk: .space 14
 
+	## Errors
+	FileNotFound: .asciiz "Could not open file"
+	FileNotRead: .asciiz "Could not read from file"
+	NoHeader: .asciiz "There was no header chunk found"
+
 .text
 .globl main
 main:
-
 	# read from file, file descriptor goes into $v0
 	jal open_file
 
 
 	# if file descriptor less than 0, error
-	lw $a0  FileNotFound
+	la $a0  FileNotFound
 	blt $a0 $zero exit_with_error
 
-	mv $s7  $v0 # move file descriptor into s0
+	move $s7  $v0 # move file descriptor into s0
 
 
 	# Read from the file descriptor
-	mv $a0  $s7         # file descriptor
-	lw $a1  HeaderChunk # buffer location
+	move $a0  $s7       # file descriptor
+	la $a1  HeaderChunk # buffer location
 	li $a2  12          # maximum number of characters to read
 	li $v0  14          # 13 is for opening files
 	syscall
 
-
+	# Parse the header
 	jal parse_header
 
-li $v0 10
-syscall
+
+	li $v0 10
+	syscall
+
 
 ################################################################################
 
 
 	## Open a file for reading, file descriptor will go into $v0
 open_file:
-	lw $a0  FileName # filename
+	la $a0  FileName # filename
 	li $a1  0x0      # flags
 	li $a2  0x0      # mode, 0 for read
 	li $v0  13       # 13 is for opening files
 	syscall
 
-	j $ra
+	jr $ra
 
 
 ################################################################################
@@ -63,6 +69,7 @@ open_file:
 	## been filled with meaningful data by reading from a midi file
 parse_header:
 
+	jr $ra
 
 
 ################################################################################
--- src/util.asm
+++ src/util.asm
@@ -1,24 +1,18 @@
 
-.data
-	## Errors
-	FileNotFound: .asciiz "Could not open file"
-	FileNotRead: .asciiz "Could not read from file"
-	NoHeader: .asciiz "There was no header chunk found"
-
-
 .text
 	## Decodes some byte sequence into a number based on
 	## http://www.music.mcgill.ca/~ich/classes/mumt306/StandardMIDIfileformat.html#BM1_
 	## 
 	## $a0: address of the starting byte of the number to be decoded
 	## $v0: computed number
+.globl decode_var_len
 decode_var_len:
 	# Set the return value to 0 initially
 	li $v0 0
 
 	# Fill registers with stuff i need
 	li $t0 0   # Byte we're currently reading
-	mv $t1 $a0 # Position of the byte we're reading
+	move $t1 $a0 # Position of the byte we're reading
 
 _loop:
 	sll $v0  $v0 7 # Shift the accumulated value by 7 bits
@@ -33,7 +27,7 @@ _loop:
 	bne $t2 $zero _loop # repeat the loop if we have a 1 at the end
 
 
-	j $ra
+	jr $ra
 
 
 ################################################################################
@@ -43,6 +37,7 @@ _loop:
 	##
 	## $a0: address of the starting byte of the chunk
 	## $v0: length of the chunk
+.globl read_chunk
 read_chunk: 
 	# Skip over the 4 bytes for the chunk type and read the 32 bit length of the
 	# chunk
@@ -55,8 +50,7 @@ read_chunk:
 	## Exit and print out an error
 	##
 	## $a0: address of null terminated string to exit with
+.globl exit_with_error
 exit_with_error: 
-
-
 	li $v0 10
 	syscall
