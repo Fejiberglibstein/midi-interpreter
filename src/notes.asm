463e783ae71b07285e04f3a907cdd7bc8503083c
Austin Thein <amthein321@gmail.com>
2024-12-30 12:14:39 -0500

    fixup! Get some documentation going

--- src/notes.asm
+++ src/notes.asm
@@ -1,4 +1,17 @@
 
+.data
+	## Pointer to the list of notes. This list will be heap allocated by
+	## repeatedly calling `sbrk`. Each note is 2 words long. The contents of a
+	## note are as follows:
+	##	
+	##	struct note {
+	##		word start; // The millisecond the note started on. 
+	## 		byte channel;    // The channel number of the note
+	##		byte key;   
+	## 		byte instrument; 
+	## 		byte volume;    
+	##	}
+	Notes: .word 0
 
 .text
 
@@ -9,3 +22,4 @@
 	## $a2: key of the note to play
 .globl add_note
 add_note:
+
